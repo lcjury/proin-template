fde7c50db54b2c5931311bd3caef1ac6055f3b74
Jury <zeui16@gmail.com>
2016-03-16 21:06:29 -0300

    Updated front-page chart data

--- js/front-page.js
+++ js/front-page.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
 
 $(document).ready(function(){
 var data = {
-    labels: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio"],
+    labels : [ "Septiembre","Octubre","Noviembre","Diciembre","Enero","Febrero","Marzo" ],
     datasets: [
         {
             fillColor: "rgba(151,187,205,0.2)",
@@ -21,7 +21,7 @@ var data = {
             pointStrokeColor: "#fff",
             pointHighlightFill: "#fff",
             pointHighlightStroke: "rgba(151,187,205,1)",
-            data: [10, 22, 26, 32, 42, 52, 60]
+            data : [ "474","869","1246","1554","1913","2286","2549" ]
         }
     ]
 };
@@ -30,4 +30,3 @@ var ctx = jQuery("#myChart").get(0).getContext("2d");
 // This will get the first returned node in the jQuery collection.
 var myNewChart = new Chart(ctx).Line(data, {responsive: true,scaleShowVerticalLines: false,});
 });
-
